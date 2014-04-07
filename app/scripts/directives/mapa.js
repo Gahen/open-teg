392b0b82a3dcfbce8a8902e08e014f016adcdb8f
Joaquín Omar Tumas <j.tumas@gmail.com>
2014-04-07 02:29:11 -0300

    mapa directive, removed vim tempfiles and working click and hover for the map!

---
+++ app/scripts/directives/mapa.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('tegApp')
+	.directive('mapa', function () {
+		return {
+			templateUrl: 'images/mapa.svg',
+			restrict: 'E',
+			link: function postLink(scope, element, attrs) {
+				var box = angular.element('<div></div>');
+				element.after(box);
+				element.find('g').find('g')
+					.on('click', function(el){
+						box.text(el.currentTarget.id);
+					});
+			}
+		};
+	});
